4044fb8a785c87f1c511ddc9aeb8c1ba4fe310a5
nada <nada.h.farouk@gmail.com>
2019-02-13 15:07:01 +0200

    resources that we might need

--- travel/travel/settings.py
+++ travel/travel/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'travel_db',
         'USER':'root',
-        'PASSWORD':'',
+        'PASSWORD':'P@$$w0rd_',
         'HOST':'127.0.0.1',
         'PORT':'3306'
 	}
--- travel/user_profile/models.py
+++ travel/user_profile/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class User(models.Model):
     user_name =models.CharField(max_length =100)
     name =models.CharField(max_length =100)
-     password=models.CharField(max_length =100)
-    is_admin=models.BooleanField(initial=False)
-    status=models.BooleanField(initial=False)
+    password=models.CharField(max_length =100)
+    is_admin=models.BooleanField()
+    status=models.BooleanField()
     def __str__(self):
         return self. name
 
