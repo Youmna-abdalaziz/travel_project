a68a56acbd787fd05d26982f8c6d36002c3df88b
nada <nada.h.farouk@gmail.com>
2019-02-15 19:03:22 +0200

    userprofile app in its 1st version

--- travel/user_profile/models.py
+++ travel/user_profile/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 
-class User(models.Model):
-    user_name =models.CharField(max_length =100)
-    name =models.CharField(max_length =100)
-    password=models.CharField(max_length =100)
-    is_admin=models.BooleanField()
-    status=models.BooleanField()
+class ExtendedUser(models.Model):
+    user=models.OneToOneField(User)
+    details=models.CharField(max_length =500)
+   
     def __str__(self):
-        return self. name
+        return self.user.username
 
+    def create_profile(sender , **kwargs):
+        if kwargs['created']:
+            user_profile=ExtendedUser.objects.create(user=kwargs['instance'])    
+
+   
+post_save.connect(ExtendedUser.create_profile, sender=User)
   
